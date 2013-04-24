45873d145f767dae7e465dd8e9ab4aa0e18d5b61
Wim Muskee <wimmuskee@gmail.com>
2013-04-24 11:23:31 +0200

    Fix some bugs.

--- libs/functions.sh
+++ libs/functions.sh
@@ -42,7 +42,7 @@ function checkHttpStatus {
 function getRecords {
 	# download the oaipage
 	local starttime=$(date +%s%N | cut -b1-13)
-	curl ${CURL_OPTS} ${URL} -O ${TMP}/oaipage.xml
+	curl ${CURL_OPTS} ${URL} -o ${TMP}/oaipage.xml
 	local endtime=$(date +%s%N | cut -b1-13)
 	local downloadtime=$(echo "scale=3; ($endtime - $starttime)/1000" | bc)
 
