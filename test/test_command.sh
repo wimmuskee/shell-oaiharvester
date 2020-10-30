814ff88f3b0a803d69aa2e610bd2f662f671acd6
Wim Muskee <wimmuskee@gmail.com>
2020-10-30 19:23:44 +0100

    remove getopts from option reading

--- test/test_command.sh
+++ test/test_command.sh
@@ -14,9 +14,19 @@ function testVersion {
 	assertPattern "${TESTCLASS}/${FUNCNAME[0]}/1" "$(./oaiharvester -v)" "^[0-9]\.[0-9]\.[0-9]$"
 }
 
+function testNoArgs {
+	assertEqual "${TESTCLASS}/${FUNCNAME[0]}/1" "$(./oaiharvester 2>&1)" "Error: No repository id provided"
+}
+
+function testWrongArg {
+	assertEqual "${TESTCLASS}/${FUNCNAME[0]}/1" "$(./oaiharvester -x 2>&1)" "Error: ./oaiharvester: Unknown command parameter"
+}
+
 # call the functions
 testList
 testVersion
+testNoArgs
+testWrongArg
 
 # and change back to test dir for other tests
 cd test
